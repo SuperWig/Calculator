a15e7c305f8173584ec42fc9e00362d465d45cec
SuperWig <xandan@gmail.com>
2014-02-23 10:42:25 +0000

    Fixed calculation logic
    - fixed the problem where negative numbers weren't taken into account
    correctly for * and /
    - fixed the problem where adding a negative number would cause an
    infinite loop or other problems
    - fixed the issue when having a negative sign in element 0
    
    - added std::move and replace for insertion

--- main.cpp
+++ main.cpp
@@ -12,14 +12,19 @@ int main()
 
 	std::cout << "\tWelcome to the basic calculator!\n" << std::endl;
 	std::cout << "You can perform a basic operation(/ * - + ^)" << std::endl;
-	std::cout << "Please enter your task:" << std::endl;
-	std::getline(std::cin, inputString);
-
-	calculateInput(inputString);
-	sum = std::stod(inputString);
-
-	std::cout << "Total is: " << sum << std::endl;
-
+	char repeat{ 'a' };
+	while (repeat != 'n')
+	{
+		std::cout << "Please enter your task:" << std::endl;
+		std::getline(std::cin, inputString);
+		calculateInput(inputString);
+		sum = std::stod(inputString);
+		std::cout << "Total is: " << sum << std::endl;
+		std::cout << "Calculator another?" << std::endl;
+		std::cin >> repeat;
+		std::cin.clear();
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+	}
 	return 0;
 }
 
@@ -44,8 +49,7 @@ void calculateExpression(std::string& str)
 		tempL = std::pow(tempL, tempR);
 		//replace the expression with the answer
 		pos = str.find_first_not_of("1234567890.-", pos);
-		str.erase(begin, pos - begin);
-		str.insert(begin, std::to_string(tempL));
+		std::move(str.replace(begin, pos - begin, std::to_string(tempL)));
 		//find next power
 		pos = str.find_first_of("^", 0);
 	}
@@ -54,8 +58,11 @@ void calculateExpression(std::string& str)
 	{
 		//find the lhs begining of the expression by finding a non number
 		auto begin = str.find_last_not_of("0123456789.", (pos - 1));
+		if (begin == std::string::npos)
+			begin++;
 		//increase it by one to get the number
-		begin++;
+		if (str[begin] != '-' && begin != 0)
+			begin++;
 		//convert the numbers into doubles
 		tempL = std::stod(str.substr(begin));
 		tempR = std::stod(str.substr(++pos));
@@ -64,28 +71,47 @@ void calculateExpression(std::string& str)
 		else if (str[pos - 1] == '/')
 			tempL /= tempR;
 		//replace the expression with the answer
-		pos = str.find_first_not_of("1234567890.-", pos);
-		str.erase(begin, pos - begin);
-		str.insert(begin, std::to_string(tempL));
+		while (!isdigit(str[pos]))
+			pos++;
+		pos = str.find_first_not_of("1234567890.", pos);
+		//str.erase(begin, pos - begin);
+		//str.insert(begin, std::to_string(tempL));
+		std::move(str.replace(begin, pos - begin, std::to_string(tempL)));
 		//find the next factor
 		pos = str.find_first_of("*/", 0);
 	}
 	//repeat for "+" and "-"
 	pos = str.find_first_of("+-", 0);
-	while (pos != std::string::npos && str[0] != '-')
+	while (pos != std::string::npos)
 	{
-		auto begin = str.find_last_not_of("1234567890.", (pos - 1));
-		begin++;
-		tempL = std::stod(str.substr(begin));
-		tempR = std::stod(str.substr(++pos));
-		if (str[pos - 1] == '+')
-			tempL += tempR;
-		else if (str[pos - 1] == '-')
-			tempL -= tempR;
-		pos = str.find_first_not_of("1234567890.", pos);
-		str.erase(begin, pos - begin);
-		str.insert(begin, std::to_string(tempL));
-		pos = str.find_first_of("+-", 0);
+		//if the starting position is a minus number ingore it and find the next
+		if (pos == 0 && str[0] == '-')
+			pos = str.find_first_of("+-", 1);
+		else
+		{
+			//find the starting position of the lhs of the operation
+			//negative number will result in npos
+			auto begin = str.find_last_not_of("1234567890.-", (pos - 1));
+			begin++;
+			//create doubles for lhs and rhs
+			tempL = std::stod(str.substr(begin));
+			tempR = std::stod(str.substr(++pos));
+			//perform the calculation
+			if (str[pos - 1] == '+')
+				tempL += tempR;
+			else if (str[pos - 1] == '-')
+				tempL -= tempR;
+			//find a digit after the operation
+			while (!isdigit(str[pos]))
+				pos++;
+			//find the end of the string to delete
+			pos = str.find_first_not_of("1234567890.", pos);
+			std::move(str.replace(begin, pos - begin, std::to_string(tempL)));
+			if (str[0] == '-')
+				pos = str.find_first_of("+-", 1);
+			else
+				pos = str.find_first_of("+-", 0);
+		}
 	}
 }
 
@@ -98,8 +124,7 @@ void calculateInput(std::string& str)
 	{
 		auto subString = str.substr(front + 1, back - front - 1);
 		calculateExpression(subString);
-		str.erase(front, back - front + 1);
-		str.insert(front, subString);
+		std::move(str.replace(front, back - front + 1, subString));
 		front = str.find_last_of("(", back);
 		back = str.find_first_of(")", front);
 	}
